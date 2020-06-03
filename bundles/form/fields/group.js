39af50beedd1f4b302c1488ba2f7a79b39cbc4b5
edencoder <alex@edenup.com>
2020-06-03 23:23:43 +1000

    amend fields

--- bundles/form/fields/group.js
+++ bundles/form/fields/group.js
@@ -98,6 +98,8 @@ class GroupField {
     field.value = await Promise.all((field.value || []).map(async (item) => {
       // built form
       const newForm = await formHelper.get(field.uuid);
+      
+      console.log('value only', valueOnly);
 
       // check value only
       if (!valueOnly) {
