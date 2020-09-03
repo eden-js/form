bfbbfb7e93eaaeff8c0ddf4598568df9391d5ba1
edencoder <alex@edenup.com>
2020-09-03 16:40:30 +1000

    amend issues

--- bundles/form/models/form.ts
+++ bundles/form/models/form.ts
@@ -17,16 +17,17 @@ export default class Form extends Model {
   async sanitise(req, current = []) {
     // data
     const data = {};
+    const fields = this.get('fields') || [];
 
     // req
     if (req) {
       // loop fields
-      await Promise.all((this.get('fields') || []).map(async (field) => {
+      await Promise.all(fields.map(async (field) => {
         // get from register
         const registered = fieldHelper.fields().find(b => b.type === field.type);
 
         // get value
-        const value = await registered.value(req, field, (current.find((c) => {
+        const value = registered && await registered.value(req, field, (current.find((c) => {
           // return found field
           return c.uuid === field.uuid;
         }) || {}).value);
@@ -39,10 +40,10 @@ export default class Form extends Model {
     // return placement
     return {
       data,
+      fields,
       id        : this.get('_id') ? this.get('_id').toString() : null,
       name      : this.get('name'),
       action    : this.get('action'),
-      fields    : this.get('fields') || [],
       placement : this.get('placement'),
     };
   }
