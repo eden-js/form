59ec0a0d05232e4304d903e9cea1301014ce5509
edencoder <alex@edenup.com>
2021-01-12 13:05:22 +1000

    image as url

--- bundles/form/fields/image.ts
+++ bundles/form/fields/image.ts
@@ -1,6 +1,7 @@
 
 // import field interface
 import Field from 'field';
+import isURL from 'is-url';
 
 // require models
 const Image = model('image');
@@ -66,6 +67,82 @@ export default class ImageField extends Field {
     return await Promise.all(value.filter(val => val).map(async (val, i) => {
       // item
       if (val && val.id) val = val.id;
+
+      // val is not an id
+      if (!`${val}`.match(/^[0-9a-fA-F]{24}$/) && isURL(val)) {
+        // run try catch
+        try {
+          // create new upload
+          const image = new Image();
+
+          // upload
+          await image.fromURL(val);
+
+          // images
+          const images = [
+            (await image.thumb('1x')).resize(400, 400, {
+              fit : 'contain',
+            }).png().save(),
+            (await image.thumb('2x')).resize(800, 800, {
+              fit : 'contain',
+            }).png().save(),
+            (await image.thumb('3x')).resize(1200, 1200, {
+              fit : 'contain',
+            }).png().save(),
+            (await image.thumb('sm-sq')).resize(400, 400, {
+              fit        : 'contain',
+              background : {
+                r     : 0,
+                g     : 0,
+                b     : 0,
+                alpha : 0,
+              },
+            }).png().save(),
+            (await image.thumb('md-sq')).resize(800, 800, {
+              fit        : 'contain',
+              background : {
+                r     : 0,
+                g     : 0,
+                b     : 0,
+                alpha : 0,
+              },
+            }).png().save(),
+            (await image.thumb('lg-sq')).resize(1200, 1200, {
+              fit        : 'contain',
+              background : {
+                r     : 0,
+                g     : 0,
+                b     : 0,
+                alpha : 0,
+              },
+            }).png().save(),
+            (await image.thumb('1x-sq')).resize(400, 400, {
+              fit : 'cover',
+            }).png().save(),
+            (await image.thumb('2x-sq')).resize(800, 800, {
+              fit : 'cover',
+            }).png().save(),
+            (await image.thumb('3x-sq')).resize(1200, 1200, {
+              fit : 'cover',
+            }).png().save(),
+          ];
+      
+          // await all thumbs
+          await Promise.all(images);
+      
+          // set user
+          image.set('user', req.user);
+
+          // return sanitised
+          return {
+            id    : image.get('_id'),
+            model : 'image',
+          };
+        } catch (e) {
+          // return old
+          return old[i];
+        }
+      }
 
       // run try catch
       try {
